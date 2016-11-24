2f30579671f92377ef84a0cf1a502266616f7a2c
Serwerty <oleg.dovzhenko2012@gmail.com>
2016-11-24 20:08:06 +0200

    css improvements

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 echo "<footer>";
 echo '<div class = "footer_container">';
 echo '<a href="https://github.com/Serwerty/Store">GitHub</a>';
-echo '<p>Jus Santé © 2016 Created and desined by Zulfia Hamaeva and Oleg Dovzhenko</p>';
+echo '<p>© 2016 Jus Santé Created and designed by Zulfia Hamaeva and Oleg Dovzhenko.</p>';
 echo "</div>";
 echo "</footer>";
 ?>
